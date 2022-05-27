e87d42c3500a3999238c0824958922e42b04d00e
Abhinav <abhinavkumr77@gmail.com>
2022-05-27 15:46:39 +0530

    New component for Input field

---
+++ src/components/InputForm.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+
+export default function InputForm(props) {
+    
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    
+    const handleLowerClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+    
+    const [text, setText] = useState('Enter Text');
+
+  return (
+      <>
+    <div className='container'>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+            <div className="form-group">
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="textArea" rows={10} ></textarea>
+            </div>
+            <button className="btn btn-primary mx-2" onClick = {()=> handleUpClick()}>Convert UpperCase Text</button>
+            <button className="btn btn-primary mx-2" onClick = {()=> handleLowerClick()}>Convert LowerCase Text</button>
+        </div>
+    </div>
+    <div className="container my-2">
+        <h1>Your Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{(0.008 * text.split(" ").length).toFixed(2)} Minute read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+    </div>
+    </>
+  )
+}
