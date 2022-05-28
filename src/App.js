57d976ced278b7238e7667e856b0326f3dd372aa
Abhinav <abhinavkumr77@gmail.com>
2022-05-28 13:18:04 +0530

    color mode change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
+import { useState } from 'react';
+import About from './components/About';
 import InputForm from './components/InputForm';
 import Navbar from './components/Navbar';
 function App() {
+  const [mode, setMode] = useState('light');
   let prop = {
     title: "Text Polisher",
     aboutText: "About Us",
-    home: "Home"
+    home: "Home",
+    mode: mode,
+    toggleMode:  () => {
+      if(mode === 'light') {
+        setMode('dark');
+        document.body.style.backgroundColor = 'grey';
+      }
+
+      else {
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+    }
+    }
   }
   let val = <>     
               <Navbar {...prop} />
               <div className="container my-3">
-              <InputForm heading = "Enter text to Amend"/>
+              <InputForm {...prop} heading = "Enter text to Amend"/>
+              {/* <About/> */}
               </div>
               
             </>
