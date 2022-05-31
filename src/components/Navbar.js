9a7bcab62f36228afefcc56613b7ce2677e5a746
Abhinav <abhinavkumr77@gmail.com>
2022-05-31 22:06:59 +0530

    a changed to Link for react router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom'
 let Navbar = (props) => {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-        <a className="navbar-brand" href="#">{props.title}</a>
+        <Link className="navbar-brand" to="/">{props.title}</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
@@ -11,10 +12,10 @@ let Navbar = (props) => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-                <a className="nav-link" href="#">{props.home}<span className="sr-only">(current)</span></a>
+                <Link className="nav-link" to="/">{props.home}<span className="sr-only">(current)</span></Link>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">{props.aboutText}</a>
+                <Link className="nav-link" to="/About">{props.aboutText}</Link>
             </li>
             </ul>
             {/* <form className="form-inline my-2 my-lg-0">
