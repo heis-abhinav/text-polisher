003e59195f1ce170ccb6cc31e7f6e6f9024f16b2
Abhinav <abhinavkumr77@gmail.com>
2022-05-31 22:05:31 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import About from './components/About';
 import InputForm from './components/InputForm';
 import Navbar from './components/Navbar';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+
   let prop = {
     title: "Text Polisher",
     aboutText: "About Us",
@@ -14,21 +35,34 @@ function App() {
       if(mode === 'light') {
         setMode('dark');
         document.body.style.backgroundColor = 'grey';
+        showAlert("Dark mode has been enabled", "success");
       }
 
       else {
         setMode('light');
         document.body.style.backgroundColor = 'white';
+        showAlert("Light mode has been enabled", "success");
     }
     }
   }
   let val = <>     
+            <Router>
               <Navbar {...prop} />
+              <Alert alert = {alert}></Alert>
               <div className="container my-3">
-              <InputForm {...prop} heading = "Enter text to Amend"/>
+              
+                <Routes>
+                  {/* <Route path="/about">
+                    <About />
+                  </Route> */}
+                  <Route path="/" element = {<InputForm {...prop} heading = "Enter text to Amend"/>} />
+                  <Route path="/About" element = {<About/> } />
+                
+                </Routes>
+              
               {/* <About/> */}
               </div>
-              
+              </Router>
             </>
   return (
     val
