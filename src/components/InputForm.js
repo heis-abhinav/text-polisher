f174cdacc795fc60f8f5cba536732938f9716a7b
Abhinav <abhinavkumr77@gmail.com>
2022-05-31 22:06:24 +0530

    zero length word removed

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -26,13 +26,13 @@ export default function InputForm(props) {
             <div className="form-group">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="textArea" rows={10} style = {{backgroundColor: props.mode ==='light'? 'white': 'grey', color: props.mode ==='dark'? 'white': 'black' }} ></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick = {()=> handleUpClick()}>Convert UpperCase Text</button>
-            <button className="btn btn-primary mx-2" onClick = {()=> handleLowerClick()}>Convert LowerCase Text</button>
+            <button className="btn btn-primary mx-2 my-2" onClick = {()=> handleUpClick()}>Convert UpperCase Text</button>
+            <button className="btn btn-primary mx-2 my-2" onClick = {()=> handleLowerClick()}>Convert LowerCase Text</button>
         </div>
     </div>
     <div className="container my-2" style = {{color: props.mode ==='dark'? 'white': 'black'}}>
         <h1>Your Summary</h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.split(" ").filter((element) => {return element.length!=0}).length} words and {text.length} characters</p>
         <p>{(0.008 * text.split(" ").length).toFixed(2)} Minute read</p>
         <h3>Preview</h3>
         <p>{text.length > 0 ? text: "Enter something to preview"}</p>
